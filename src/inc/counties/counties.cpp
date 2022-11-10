902d046041a9b59aeb0d41ae8c952a4d4587c7e3
andreisdc <101742390+andreisdc@users.noreply.github.com>
2022-11-10 17:59:22 +0200

    Counties implementation
    Am implementat clasa counties.

---
+++ src/inc/counties/counties.cpp
@@ -0,0 +1,6 @@
+#include "counties.h"
+
+counties::counties(std::string name, int exp) {
+	this->m_countiesName = name;
+	this->m_exp = exp;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "counties/counties.h"
 
 int main(){
 	return 0;
