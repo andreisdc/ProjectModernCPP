79c6521c50f7f9c1aa6f638ccba0d221cc2b5d16
Theodor Stamati <16341325+itstheodor@users.noreply.github.com>
2022-11-27 23:56:11 +0200

    Lay out basic user-related routes.

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -2,13 +2,61 @@
 #include <crow.h>
 #include <sqlite_orm/sqlite_orm.h>
 
+struct User {
+	int id;
+	std::string username;
+	std::string password;
+};
+
 int main() {
 	crow::SimpleApp app;
+
+	using namespace sqlite_orm;
+
+	auto storage = make_storage(
+		"db.sqlite",
+		make_table(
+			"users",
+			make_column("id", &User::id, autoincrement(), primary_key()),
+			make_column("username", &User::username),
+			make_column("password", &User::password)
+		)
+	);
 
 	CROW_ROUTE(app, "/")
 		([]() {
 			return "Hello world";
 		});
+
+	CROW_ROUTE(app, "/users/login")
+		.methods(crow::HTTPMethod::POST)
+			([&storage](const crow::request &req) {
+				auto x = crow::json::load(req.body);
+				if (!x)
+					return crow::response(400);
+
+				std::string username = x["username"].s();
+				std::string password = x["password"].s();
+
+				return crow::response(200);
+			});
+
+	CROW_ROUTE(app, "/users/register")
+		.methods(crow::HTTPMethod::POST)
+			([&storage](const crow::request &req) {
+				auto x = crow::json::load(req.body);
+				if (!x)
+					return crow::response(400);
+
+				std::string username = x["username"].s();
+				std::string password = x["password"].s();
+
+				User user{-1, username, password};
+				auto newUserId = storage.insert(user);
+				user.id = newUserId;
+
+				return crow::response(201);
+			});
 
 	app.port(8080)
 		.multithreaded()
