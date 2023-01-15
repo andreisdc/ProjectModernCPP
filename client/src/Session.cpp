f1a1fc2e922381ddb4d8aaf0214081ffbf40e3e1
andreisdc <101742390+andreisdc@users.noreply.github.com>
2023-01-15 14:31:04 +0200

    Modified Session

--- client/src/Session.cpp
+++ client/src/Session.cpp
@@ -1,6 +1,13 @@
 #include "Session.hpp"
-
 Session::Session(User *player1, User *player2, User *player3, User *player4) {
+	if (player1 != nullptr)
+		m_nrPlayer++;
+	if (player2 != nullptr)
+		m_nrPlayer++;
+	if (player3 != nullptr)
+		m_nrPlayer++;
+	if (player4 != nullptr)
+		m_nrPlayer++;
 	this->m_player1 = player1;
 	this->m_player2 = player2;
 	this->m_player3 = player3;
@@ -23,14 +30,34 @@ void Session::setPoint() {
 	m_player3->setPoints(0);
 	m_player4->setPoints(0);
 }
-void Session::addPlayer(int nrPlayer) {
-
-	 if (nrPlayer == 2) {
-		m_player1 = nullptr;
-	} else if (nrPlayer == 3) {
+void Session::kickPlayer(int nrPlayer) {
+	if (nrPlayer == 2) {
 		m_player2 = nullptr;
-	} else if (nrPlayer == 4) {
+		m_nrPlayer--;
+	}
+	if (nrPlayer == 3) {
 		m_player3 = nullptr;
+		m_nrPlayer--;
+	}
+	if (nrPlayer == 4) {
+		m_player4 = nullptr;
+		m_nrPlayer--;
 	}
 }
+int Session::getNumberPlayer() {
+	return m_nrPlayer;
+}
+void Session::Verify(int nrPlayer) {
+	if (m_player1 != nullptr)
+		lobby.push_back(m_player1);
+	if (m_player2 != nullptr)
+		lobby.push_back(m_player2);
+	if (m_player3 != nullptr)
+		lobby.push_back(m_player3);
+	if (m_player4 != nullptr)
+		lobby.push_back(m_player4);
+}
 
+const std::string Session::getUser(int nr) {
+	return lobby[nr - 1]->getUsername();
+}
--- client/src/Session.hpp
+++ client/src/Session.hpp
@@ -1,20 +1,42 @@
 #pragma once
 
-#include "Board.hpp"
-#include "County.hpp"
 #include "User.hpp"
-
+#include "Lobby.hpp"
 class Session {
 private:
 	User *m_player1 = nullptr;
 	User *m_player2 = nullptr;
 	User *m_player3 = nullptr;
 	User *m_player4 = nullptr;
-	int nrPlayer = 0;
+	int m_nrPlayer = 0;
+	int nrMap = 0;
+	std::vector<User *> lobby;
+
 public:
 	Session(User *player1 = nullptr, User *player2 = nullptr, User *player3 = nullptr, User *player4 =nullptr);
+
 	void getPoint(int nrPlayer);
 	void setPoint();
-	void addPlayer(int nrPlayer);
-	void kickPlayer();
+	void kickPlayer(int nrPlayer);
+	int getNumberPlayer();
+	bool isOnline(int nrPlayer) {
+		if (nrPlayer == 2) {
+			if (m_player2 == nullptr) {
+				return false;
+			}
+		}else
+		if (nrPlayer == 3) {
+			if (m_player3 == nullptr) {
+				return false;
+			}
+		}else
+		if (nrPlayer == 4) {
+			if (m_player4 == nullptr) {
+				return false;
+			}
+		}else
+		return true;
+	}
+	void Verify(int nrPlayer);
+	const std::string getUser(int nr);
 };
